c126907b03923ffba9b5a817b7adf581d578c7ef
butolin-pawel <butolin-pawel@yandex.ru>
2023-05-10 18:01:09 +0300

    +ic

--- Service/src/app/goods-catalog/goods-catalog.component.ts
+++ Service/src/app/goods-catalog/goods-catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { faCartPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-goods-catalog',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./goods-catalog.component.css']
 })
 export class GoodsCatalogComponent {
-
+  faCartPlus =faCartPlus
 }
